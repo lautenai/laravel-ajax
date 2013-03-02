cd322ffeb4053810d56d348f50e4b049016664b3
lautenai <lautenai@gmail.com>
2013-03-01 22:28:12 -0300

    first commit

---
+++ application/views/content.php
@@ -0,0 +1,3 @@
+<!-- application/views/content.php -->
+<h1>New Content</h1>
+<p>This is our AJAX loaded content.</p>
---
+++ public/js/script.js
@@ -0,0 +1,27 @@
+// public/js/script.js
+$(document).ready(function() {
+$('#load-content-get').click(function(e) {
+	// prevent the links default action
+	// from firing
+	e.preventDefault();
+	// attempt to GET the new content
+		$.get(BASE+'/content', function(data) {
+			$('#content').html(data);
+		});
+	});
+
+$('#load-content-post').click(function(e) {
+	// prevent the links default action
+	// from firing
+	e.preventDefault();
+	// attempt to GET the new content
+		// attempt a POST request with
+		// some additional data
+		$.post(BASE+'/content', {
+			name: "Dayle",
+			age: 27
+			}, function(data) {
+			$('#content').html(data);
+		});
+	})
+});
